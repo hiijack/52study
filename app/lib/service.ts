852690e55bbb64a395ff118513939dba2b0579cb
lunjz <lunjz@icloud.com>
2025-09-04 15:28:18 +0800

    fix cache matter

--- app/lib/service.ts
+++ app/lib/service.ts
@@ -4,9 +4,7 @@ import jwt from 'jsonwebtoken';
 export function queryBook(term, page): Promise<{ data; message: string; totalPages }> {
   if (term === '') {
     // todo term
-    return fetch(`/api/get-book?term=&page=${page}`, {
-      cache: 'force-cache',
-    }).then((res) => res.json());
+    return fetch(`/api/get-book?term=&page=${page}`).then((res) => res.json());
   }
   return fetch('/api/ai-search', { method: 'POST', body: JSON.stringify({ prompt: term }) }).then((res) =>
     res.json()
