deff6b93d3beb5b0f6129a0b679b4b91bbcba53b
lunjz <lunjz@icloud.com>
2025-10-22 16:25:16 +0800

    change ai model

--- app/api/ai-search/route.ts
+++ app/api/ai-search/route.ts
@@ -58,7 +58,7 @@ export async function POST(req: Request) {
     });
 
     const result: any = await generateText({
-      model: model.chatModel('qwen-plus'),
+      model: model.chatModel('qwen3-max'),
       system: 'You are a book search assistant.',
       tools: await client.tools(),
       prompt,
